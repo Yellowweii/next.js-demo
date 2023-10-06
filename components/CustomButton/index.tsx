1b772edd98729ec892d94ea6ba9b219fba13ba4e
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-06 14:40:31 +0800

    finish the hero module (#2)
    * customize button
    
    * finish the hero module
    
    ---------
    
    Co-authored-by: yellowwei <1119765689@qq.com>

--- components/CustomButton/index.tsx
+++ components/CustomButton/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
   return (
-    <button disabled={false} type="button" className={`custom-btn ${containerStyles}` } onClick={handleClick}>
+    <button disabled={false} type="button" className="custom-btn bg-primary-blue text-white rounded-full mt-10" onClick={handleClick}>
       <span className={`flex-1`}>{title}</span>
     </button>
   );
--- sections/Hero/index.tsx
+++ sections/Hero/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import CustomButton from "@/components/CustomButton";
 import Image from "next/image";
@@ -12,7 +12,12 @@ const Hero = () => {
         <p className="hero__subtitle"> Streamline your car rental experience with our effortless booking process.</p>
         <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
       </div>
-      
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
     </div>
   );
 };
