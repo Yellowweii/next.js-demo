0f4e5dbe3b52db81e811b9af726a9c3a237dda67
yellowwei <1119765689@qq.com>
2023-10-06 14:01:18 +0800

    customize button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import { Hero } from "@/sections";
 
 export default function Home() {
-  return <main className="overflow-hidden"></main>;
+  return <main className="overflow-hidden">
+    <Hero />
+  </main>;
 }
--- sections/Hero/index.tsx
+++ sections/Hero/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+
+import CustomButton from "@/components/CustomButton";
+import Image from "next/image";
 
 const Hero = () => {
+  const handleScroll = () => {};
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find, book, rent a car—quick and super easy!</h1>
+        <p className="hero__subtitle"> Streamline your car rental experience with our effortless booking process.</p>
+        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+      </div>
+      
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
