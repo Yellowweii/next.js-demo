36689ed7a8da51f3d5722c6bd981985868548446
yellowwei <1119765689hw@gmail.com>
2023-10-06 19:48:05 +0800

    finish the searchbar module

--- sections/index.ts
+++ sections/index.ts
@@ -1,5 +1,8 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, Navbar, Footer };
+export { Hero, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
