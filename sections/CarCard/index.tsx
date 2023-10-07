c454ac2eebe2704f1de9750df1d50a6c2de8eebd
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-07 11:22:44 +0800

    get car data (#6)
    * customize button
    
    * finish the hero module
    
    * finish the navbar module
    
    * finish the footer module
    
    * get the car data
    
    ---------
    
    Co-authored-by: yellowwei <1119765689@qq.com>

---
+++ sections/CarCard/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils/calc";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, drive, make, model, transmission, year } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image src="/hero.png" fill priority alt="car model" className="object-contain"  />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+            <div className="flex flex-col justify-center items-center gap-2">
+                <Image width={20} height={20} src="/steering-wheel.svg" alt="steering wheel"  />
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
